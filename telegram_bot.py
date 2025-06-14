140732af65ce510ce03778acea015b6462127662
pashga <pashga112@gmail.com>
2025-06-14 14:58:51 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot = telegram.Bot(token=os.environ["TELEGRAM_BOT_TOKEN"])
-bot.send_message(chat_id=os.environ["TELEGRAM_CHAT_ID"], text="Test message")
+bot.send_photo(chat_id=os.environ["TELEGRAM_CHAT_ID"], photo=open("images/spacex_2.jpg", "rb"))
 
